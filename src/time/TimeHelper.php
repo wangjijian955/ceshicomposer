c68f20f08e47ceb565c4a22f80593f92b290eaec
evil_name <273210440@qq.com>
2023-05-12 17:04:11 +0800

    第一个composer包

---
+++ src/time/TimeHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src\time;
+
+class TimeHelper
+{
+    /**
+     * 返回今日开始和结束的时间戳
+     *
+     * @return array
+     */
+    public static function today()
+    {
+        list($y, $m, $d) = explode('-', date('Y-m-d'));
+        return [
+            mktime(0, 0, 0, $m, $d, $y),
+            mktime(23, 59, 59, $m, $d, $y)
+        ];
+    }
+}
